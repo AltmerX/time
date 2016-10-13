00dd71ee147b31c699486c626f28678f7f2e6145
AltmerX <sabrina5426@live.com>
2016-10-13 17:15:16 -0400

    new

--- app.py
+++ app.py
@@ -9,4 +9,5 @@ def timeSinceEpoch():
     return str(time.time())
 
 if __name__ == "__main__":
-    app.run(port=sys.argv[1])
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0',port=port)
